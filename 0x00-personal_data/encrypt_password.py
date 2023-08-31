3c66aa425635d0f5d10c61f7fbe7063fdf0d7a3a
kalkidanh <kalkidanh89@gmail.com>
2023-08-31 20:46:33 +0300

    hashed password returned

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,8 +9,3 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """ Return a hashed password."""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-
-
-def is_valid(hashed_password: bytes, password: str) -> bool:
-    """ Return true if password is valid and false otherwise."""
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
