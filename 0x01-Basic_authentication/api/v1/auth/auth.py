b222fc196852e4988b8ddcb0051c53789509f7b3
kalkidanh <kalkidanh89@gmail.com>
2023-09-04 19:26:02 +0000

    updated authentication method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,8 +8,16 @@ class Auth():
     """ Define the authentication system."""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Returns False """
-        return False
+        """ Returns true if the path is not in the list. """
+        if not excluded_paths or not len(excluded_paths) or not path:
+            return True
+        for element in excluded_paths:
+            if "*" in element:
+                if path.startswith(element.replace("*", "")):
+                    return False
+        if path in excluded_paths or f'{path}/' in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """return the value of the header request Authorization"""
