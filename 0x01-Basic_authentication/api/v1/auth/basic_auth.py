0719a2d699833c1c7cf4d407959ac183f2bc1f30
kalkidanh <kalkidanh89@gmail.com>
2023-09-05 16:35:17 +0000

    added basic_auth class that inherits from auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Class that manages the API basic authentication."""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ Define the class BasicAuth that inherits from Auth."""
+    pass
