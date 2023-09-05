dd985e475b5dcc5178ce3e184652444e6a75c06a
kalkidanh <kalkidanh89@gmail.com>
2023-09-05 17:03:29 +0000

    decoded value of b64 string returned

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Class that manages the API basic authentication."""
 
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -20,3 +21,18 @@ class BasicAuth(Auth):
             return None
         header = authorization_header.split(' ')
         return header[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """returns the decoded value of a Base64 string
+        base64_authorization_header"""
+        if base64_authorization_header is None:
+            return None
+        if not type(base64_authorization_header) == str:
+            return None
+        try:
+            valid_Base64 = base64.b64decode(base64_authorization_header.encode(
+                'utf-8'))
+            return valid_Base64.decode('utf-8')
+        except Exception:
+            return None
