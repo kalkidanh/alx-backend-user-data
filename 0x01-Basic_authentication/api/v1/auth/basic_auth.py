ddaeb579fca3a3f928358d798badf1bf701961c9
kalkidanh <kalkidanh89@gmail.com>
2023-09-05 17:44:16 +0000

    updated function

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -58,11 +58,13 @@ class BasicAuth(Auth):
             return None
         if user_pwd is None or not type(user_pwd) == str:
             return None
-        users = User.search({"email": user_email})
-        for user in users:
-            if user.is_valid_password(user_pwd):
-                return user
-        return None
+        try:
+            users = User.search({"email": user_email})
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
+        except Exception:
+            return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """overloads Auth and retrieves the User instance for a request"""
