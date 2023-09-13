8055cd92bfc4871d37ddba9f4a249c76f21f0e08
kalkidanh <kalkidanh89@gmail.com>
2023-09-13 17:39:03 +0000

    destroy session function added.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -64,3 +64,11 @@ class Auth:
             return user.email
         except NoResultFound:
             return
+
+    def destroy_session(self, user_id: int) -> None:
+        """ Function to terminate the session."""
+        try:
+            user = self._db.find_user_by(id=user_id)
+            self._db.update_user(user.id, session_id=None)
+        except NoResultFound:
+            pass
